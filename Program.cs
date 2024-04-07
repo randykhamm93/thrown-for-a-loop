67fd89f5c655576b7afea0941528059b2e986627
Randy Hamm <randykhamm@gmail.com>
2024-04-07 10:17:33 -0500

    added exceptions and error handling for product selection

--- Program.cs
+++ Program.cs
@@ -79,20 +79,35 @@ for (int i = 0; i < products.Count; i++)
     Console.WriteLine($"{i + 1}. {products[i].Name}");
 }
 
-Console.WriteLine("Please enter a product number: ");
+Product chosenProduct = null;
 
-int response = int.Parse(Console.ReadLine().Trim());
-
-while (response > products.Count || response < 1)
+while (chosenProduct == null)
 {
-    Console.WriteLine("Choose a number between 1 and 5!");
-
-    response = int.Parse(Console.ReadLine().Trim());
+    Console.WriteLine("Please enter a product number: ");
+    try
+    {
+        int response = int.Parse(Console.ReadLine().Trim());
+        chosenProduct = products[response - 1];
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please type only integers!");
+    }
+    catch (ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("Please choose an existing item only!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        Console.WriteLine("Do Better!");
+    }
 }
 
 DateTime now = DateTime.Now;
-Product chosenProduct = products[response - 1];
+
 TimeSpan timeInStock = now - chosenProduct.StockDate;
+
 Console.WriteLine(@$"You chose: 
 {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old. 
